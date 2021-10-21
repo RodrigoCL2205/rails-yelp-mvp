5302f266485c4407356854b255196869191494e2
Rodrigo Lima <rodrigo.cerqueira@gmail.com>
2021-10-21 17:14:16 -0300

    Desafio concluido

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning DB...'
+Restaurant.destroy_all
+puts 'DB Cleaned!'
+
+puts 'Creating restaurants...'
+
+20.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japonese french belgian].sample
+  )
+
+  puts "Restaurants #{restaurant.id} was created"
+end
